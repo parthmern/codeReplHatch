8a18028b47093057c73983d4429f6f7e630f25fd
parthmern <parthmern@gmail.com>
2025-10-04 20:13:45 -0400

    border and some css fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,17 @@ import VideoBackground from "./components/VideoBackground";
 
 function App() {
   return (
-    <div className="h-screen w-screen">
+    <div className=" overflow-x-hidden">
       {/* <TreeViewBasic /> */}
       {/* <TerminalComponent /> */}
       <Header />
-      <VideoBackground />
+      <div>
+        <VideoBackground />
+      </div>
+      <div
+        className="relative h-8 w-full border-x border-edge border-t border-t-[rgba(255,255,255,0.75)] 
+  before:absolute before:inset-0 before:bg-[repeating-linear-gradient(315deg,rgba(255,255,255,0.50)_0,rgba(255,255,255,0.75)_1px,transparent_0,transparent_50%)] before:bg-[length:10px_10px]"
+      ></div>
     </div>
   );
 }
--- frontend/src/components/VideoBackground.tsx
+++ frontend/src/components/VideoBackground.tsx
@@ -18,7 +18,7 @@ export default function VideoBackground() {
         loop
         muted
         playsInline
-        className="absolute -top-34 left-0 w-full object-cover"
+        className="absolute h-screen left-0 w-full object-cover"
       >
         <source src={vid} type="video/mp4" />
         Your browser does not support the video tag.
@@ -26,11 +26,21 @@ export default function VideoBackground() {
 
       {/* Overlay content */}
       <div className="relative z-10 flex h-full items-center justify-end pr-[20%]">
-        <div className="text-white text-4xl flex flex-col items-center justify-center">
+        <div className="text-white w-[300px] text-4xl flex flex-col items-center justify-center">
           <p>
             Build <span className="pixelFont">Software</span> like
           </p>
           <p>never before</p>
+          <p className="ibmFont pt-10 text-[12px] uppercase  text-center ">
+            Less bugs. More breakthroughs. Streamline development, cut the
+            noise, and focus on what matters: building great software.
+          </p>
+          <button
+            role="link"
+            className="relative cursor-pointer pt-5 ibmFont uppercase text-[12px] after:absolute after:bottom-[-3px] after:left-0 after:h-[1px] after:w-full after:origin-bottom-right after:scale-x-0 after:bg-white after:transition-transform after:duration-300 after:ease-[cubic-bezier(0.65_0.05_0.36_1)] hover:after:origin-bottom-left hover:after:scale-x-100"
+          >
+            Explore Now
+          </button>
         </div>
       </div>
     </div>
